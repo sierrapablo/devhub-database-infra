9aa37aba434abb7233f9df9524047e688d84ad3e
Pablo Sierra Lorente <pablosierralorente@gmail.com>
2026-01-08 00:24:42 +0100

    feat: prepare providers and structure

--- terraform/modules/postgres/versions.tf
+++ terraform/modules/postgres/versions.tf
@@ -1,6 +1,4 @@
 terraform {
-  required_version = ">= 1.13"
-
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,8 +1,1 @@
-# Example variables
-# Use this file to define variables for your Terraform configuration.
 
-variable "example" {
-  type        = string
-  default     = "Hello from Terraform variables.tf!"
-  description = "Example variable description"
-}
